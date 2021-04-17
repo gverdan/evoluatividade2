33a312a2e67fe47eaf9dc8183ad87ac4e9daa003
Gabriel Arreco <>
2021-04-17 21:57:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+int primeiro=1;
+Segundo=2;
+Terceiro=3;
+Quarto=4;
+Quinto=5;
+Sexto=6;
+Sétimo=7;
+Oitavo=8;
+char vet[]={‘a’,’b’,’c’};
+string X;
+
+Cout << “Digite a palavra X com letras minúsculas com no máximo 50 caracteres” << endl;
+getline (cin, X);
+if (X.lenght() < 50) {
+cout <<”A palavra escrita foi:” << X << endl;
+
+}
