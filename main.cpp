17f1f743920257ee16fd5952bb953e47038926eb
Gabriel Arreco <>
2021-04-18 18:46:46 +0000

    declarado todos os vetores de forma crescente.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,14 @@ int Quinto=1;
 int Sexto=1;
 int Sétimo=1;
 int Oitavo=1;
-char vet[]={‘a’,’b’,’c’};
+char vet1[]={'a','b','c'};
+char vet2[]={'d','e','f'};
+char vet3[]={'g','h','i'};
+char vet4[]={'j','k','l'};
+char vet5[]={'m','n','o'};
+char vet6[]={'p','q','r','s'};
+char vet7[]={'t','u','v'};
+char vet8[]={'w','x','y','z'};
 string X;
 
 Cout << “Digite a palavra X com letras minúsculas com no máximo 50 caracteres” << endl;
@@ -21,20 +28,18 @@ getline (cin, X);
 if (X.lenght() < 50) 
 {
 cout <<”A palavra escrita foi:” << X << endl;
-for (int i=0; i< X.size(); i++) {
-  for(int j=0; j<3; j++){
-    if(vet[j]==X[i]){
-      cout <<"#2="<< cont1 <<endl;
+for (int i=0;i<X.size();i++){
+  for(int j=0;j<3;j++){
+    if(vet1[j]==X[i]){
+      cout <<"#1="<< primeiro <<endl;
       break;
     }
-    primeiro=primeiro+1;
-  }
   primeiro=primeiro+1;
 }
 primeiro=1;
 for (int k=0; k<3;k++){
-  if(vet==P[i]){
-    cout <<"#1=" << segundo << endl;
+  if(vet2[k]==P[i]){
+    cout <<"#2=" << segundo << endl;
     brek;
   }
 
